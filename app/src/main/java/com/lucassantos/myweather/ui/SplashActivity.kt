7f3be65480173eb8c39b558e92249c78efa6665c
zLuCaS2K <JoseLucas50000@Gmail.com>
2021-06-21 16:15:16 -0300

    Implementação da SplashActivity

--- app/src/main/java/com/lucassantos/myweather/ui/SplashActivity.kt
+++ app/src/main/java/com/lucassantos/myweather/ui/SplashActivity.kt
@@ -1,13 +1,34 @@
 package com.lucassantos.myweather.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.lucassantos.myweather.R
+import com.lucassantos.myweather.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        mBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        animateTextNameApp()
+        goMainActivity()
+    }
+
+    private fun animateTextNameApp() {
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade)
+        mBinding.textNameApp.startAnimation(animation)
+    }
+
+    private fun goMainActivity() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            Toast.makeText(this, "Teste", Toast.LENGTH_SHORT).show()
+        }, 1200)
     }
 }
