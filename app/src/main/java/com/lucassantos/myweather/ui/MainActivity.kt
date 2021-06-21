b219878b6959a76b029cdbe44bbe4adc12c1d884
zLuCaS2K <JoseLucas50000@Gmail.com>
2021-06-21 16:19:23 -0300

    Inicio da implementação da MainActivity

---
+++ app/src/main/java/com/lucassantos/myweather/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.lucassantos.myweather.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lucassantos.myweather.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
