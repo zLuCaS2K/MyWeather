d1a825297580fc456174037aff7d4123a0c1df32
zLuCaS2K <JoseLucas50000@Gmail.com>
2021-06-22 13:18:54 -0300

    Implementação inicial da MainActivity completa

--- app/src/main/java/com/lucassantos/myweather/ui/MainActivity.kt
+++ app/src/main/java/com/lucassantos/myweather/ui/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.lucassantos.myweather.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lucassantos.myweather.R
+import androidx.appcompat.app.AppCompatActivity
+import com.lucassantos.myweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
 }
