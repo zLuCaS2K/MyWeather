09f763d64d67a2d247823a00eedf20908678dda9
zLuCaS2K <JoseLucas50000@Gmail.com>
2021-07-04 21:28:51 -0300

    Adicionado dependência do Glide

--- app/src/main/java/com/lucassantos/myweather/utils/Constants.kt
+++ app/src/main/java/com/lucassantos/myweather/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.lucassantos.myweather.utils
 object Constants {
 
     object API {
-        const val API_KEY = "YOUR-API-KEY"
+        const val API_KEY = "54f4974233d0f3f0e5c05c3486b2596b"
         const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     }
 }
